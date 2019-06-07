7928adcfc609dacf15e46d6ecc8790527955375e
chaitanyapotti <chaitanya.potti@gmail.com>
2019-06-07 16:46:46 +0800

    ci fixes

--- scripts/updateGistIntegrity.sh
+++ scripts/updateGistIntegrity.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 git clone git@gist.github.com:733405286923fa047af4cb26d167acd4.git ~/gist
+PACKAGE_VERSION=$(cat ./src/embed.js | grep app.tor.us | head -1 | awk -F//app.tor.us/ '{ print $2 }' | sed "s/[']//g")
 cd public/
 HASH="$(cat embed.min.js | openssl dgst -sha384 -binary | openssl base64 -A)"
 cd ~/gist
-echo "<script src="https://app.tor.us/embed.min.js"
+echo "<script src="https://app.tor.us/$PACKAGE_VERSION/embed.min.js"
         integrity=\"sha384-$HASH\"
         crossorigin="anonymous"></script>" > torus-embed.html
 
--- scripts/uploadToAws.sh
+++ scripts/uploadToAws.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-git clone git@github.com:torusresearch/torus-website.git ~/torus-website
-PACKAGE_VERSION=$(cat ~/torus-website/app/package.json \
-  | grep buildVersion \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g')
-HASH="$(cat ./public/embed.min.js | openssl dgst -sha384 -binary | openssl base64 -A)"
+PACKAGE_VERSION=$(cat ./src/embed.js | grep app.tor.us | head -1 | awk -F//app.tor.us/ '{ print $2 }' | sed "s/[']//g")
 URL=$(echo s3://static.dev.tor.us/"$PACKAGE_VERSION" | tr -d ' ')
 aws s3 cp ./public/embed.min.js $URL/embed.min.js
