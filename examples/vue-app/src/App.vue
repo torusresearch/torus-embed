f5a9c097bdd5092cac701630e4c9ef69ee6b8063
rathishubham7 <shubhamrathi@icloud.com>
2019-09-24 12:33:08 +0800

    changes

--- examples/vue-app/src/App.vue
+++ examples/vue-app/src/App.vue
@@ -1,16 +1,26 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
+    <br />
     <button v-if="publicAddress === ''" @click="login">Login</button>
     <button v-if="publicAddress !== ''" @click="changeProvider">Change Provider</button>
     <button v-if="publicAddress !== ''" @click="getUserInfo">Get User Info</button>
     <button v-if="publicAddress !== ''" @click="logout">Logout</button>
+    <br />
+    <button v-if="publicAddress !== ''" @click="signMessage">sign_eth</button>
+    <button v-if="publicAddress !== ''" @click="signTypedData_v1">sign typed data v1</button>
+    <button v-if="publicAddress !== ''" @click="signTypedData_v3">sign typed data v3</button>
+    <button v-if="publicAddress !== ''" @click="signTypedData_v4">sign typed data v4</button>
+    <div id="console">
+      <p></p>
+    </div>
   </div>
 </template>
 
 <script>
 import Torus from '@toruslabs/torus-embed'
 import Web3 from 'web3'
+import sigUtil from 'eth-sig-util'
 
 export default {
   name: 'app',
@@ -25,7 +35,7 @@ export default {
       try {
         const torus = new Torus({
           buttonPosition: 'bottom-left'
-        });
+        })
         await torus.init({
           buildEnv: 'production',
           enableLogging: true,
@@ -35,9 +45,10 @@ export default {
             networkName: 'kovan' // optional
           },
           showTorusButton: false
-        });
-        await torus.login(); // await torus.ethereum.enable()
-        const web3 = new Web3(torus.provider);
+        })
+        await torus.login() // await torus.ethereum.enable()
+
+        const web3 = new Web3(torus.provider)
         web3.eth.getAccounts().then(accounts => {
           this.publicAddress = accounts[0]
           web3.eth.getBalance(accounts[0]).then(console.log)
@@ -50,6 +61,95 @@ export default {
         console.error(error)
       }
     },
+    console(text) {
+      document.querySelector('#console>p').innerHTML = text
+    },
+    signMessage() {
+      const address = '0x29c76e6ad8f28bb1004902578fb108c507be341b'
+      const privKeyHex = '4af1bceebf7f3634ec3cff8a2c38e51178d5d4ce585c52d6043e5e2cc3418bb0'
+      const privKey = Buffer.from(privKeyHex, 'hex')
+      const message = 'Hello, world!'
+      const msgParams = { data: message }
+
+      const signed = sigUtil.personalSign(privKey, msgParams)
+      msgParams.sig = signed
+      const recovered = sigUtil.recoverPersonalSignature(msgParams)
+      console.log(recovered == address)
+      this.console('sign message => ' + (recovered == address))
+    },
+    signTypedData_v1() {
+      const address = '0x29c76e6ad8f28bb1004902578fb108c507be341b'
+      const privKeyHex = '4af1bceebf7f3634ec3cff8a2c38e51178d5d4ce585c52d6043e5e2cc3418bb0'
+      const privKey = Buffer.from(privKeyHex, 'hex')
+      const typedData = [
+        {
+          type: 'string',
+          name: 'message',
+          value: 'Hi, Alice!'
+        },
+        {
+          type: 'uint8',
+          name: 'value',
+          value: 10
+        }
+      ]
+      const msgParams = { data: typedData }
+      const signature = sigUtil.signTypedDataLegacy(privKey, msgParams)
+      const recovered = sigUtil.recoverTypedSignatureLegacy({ data: msgParams.data, sig: signature })
+      this.console('sign typed message v1 => ' + (recovered == address))
+    },
+
+    signTypedData_v3() {
+      const typedData = {
+        types: {
+          EIP712Domain: [
+            { name: 'name', type: 'string' },
+            { name: 'version', type: 'string' },
+            { name: 'chainId', type: 'uint256' },
+            { name: 'verifyingContract', type: 'address' }
+          ],
+          Person: [{ name: 'name', type: 'string' }, { name: 'wallet', type: 'address' }],
+          Mail: [{ name: 'from', type: 'Person' }, { name: 'to', type: 'Person' }, { name: 'contents', type: 'string' }]
+        },
+        primaryType: 'Mail',
+        domain: {
+          name: 'Ether Mail',
+          version: '1',
+          chainId: 1,
+          verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC'
+        },
+        message: {
+          from: {
+            name: 'Cow',
+            wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826'
+          },
+          to: {
+            name: 'Bob',
+            wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB'
+          },
+          contents: 'Hello, Bob!'
+        }
+      }
+      this.torus.web3.currentProvider.sendAsync(
+        {
+          method: 'eth_signTypedData_v3',
+          params: [this.torus.web3.eth.accounts[0], data],
+          from: this.torus.web3.eth.accounts[0]
+        },
+        function(err, result) {
+          if (err) {
+            return console.error(err)
+          }
+          console.log(result)
+          const signature = result.result.substring(2)
+          const r = '0x' + signature.substring(0, 64)
+          const s = '0x' + signature.substring(64, 128)
+          const v = parseInt(signature.substring(128, 130), 16)
+          // The signature is now comprised of r, s, and v.
+        }
+      )
+    },
+    signTypedData_v4() {},
     requestFakeSignature() {
       const domain = [
         { name: 'name', type: 'string' },
@@ -156,5 +256,30 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#console {
+  border: 1px solid black;
+  height: 40px;
+  padding: 2px;
+  bottom: 10px;
+  position: absolute;
+  text-align: left;
+  width: calc(100% - 20px);
+  border-radius: 5px;
+}
+#console::before {
+  content: 'Console :';
+  position: absolute;
+  top: -20px;
+  font-size: 12px;
+}
+#console > p {
+  margin: 0.5em;
+}
+button {
+  height: 25px;
+  margin: 5px;
+  background: none;
+  border-radius: 5px;
 }
 </style>
