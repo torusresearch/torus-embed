6a8ea16710b9bd4347cf75a8375cd297b94b6bde
chaitanyapotti <chaitanya.potti@gmail.com>
2019-11-08 14:45:35 +0800

    Updated staging url

--- src/embed.js
+++ src/embed.js
@@ -62,7 +62,7 @@ class Torus {
       let logLevel
       switch (buildEnv) {
         case 'staging':
-          torusUrl = 'https://staging.tor.us/v0.2.3'
+          torusUrl = 'https://staging.tor.us/v0.2.6'
           logLevel = 'info'
           break
         case 'testing':
