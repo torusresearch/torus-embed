3fe18358dedf8d0eda6c6c5ebf1ee83bb051f2c6
Pedro Gomes <pedrogomes94@gmail.com>
2019-12-20 17:53:05 +0100

    refactor channel-provider

--- src/channel-provider.js
+++ src/channel-provider.js
@@ -1,9 +1,13 @@
+import { setupMultiplex } from './stream-utils'
+
 const SafeEventEmitter = require('safe-event-emitter')
 
 class TorusChannelProvider extends SafeEventEmitter {
   constructor(connectionStream) {
     super()
-    this.config = {}
+    this._config = undefined
+    this._multisigAddress = undefined
+    this._signerAddress = undefined
     this.promises = {}
     this.connectionStream = connectionStream
     this.isTorusChannelProvider = true
@@ -12,33 +16,7 @@ class TorusChannelProvider extends SafeEventEmitter {
     // super constructor
     SafeEventEmitter.call(self)
 
-    const channelMux = setupMultiplex(this.connectionStream)
-    channelMux.setMaxListeners(20)
-
-    this.channelRpcStream = communicationMux.getStream('channel_rpc')
-
-    this.channelRpcStream.on('data', payload => this.onPayload(payload))
-  }
-
-  async onPayload(payload) {
-    const { id } = payload
-    if (typeof id !== 'undefined') {
-      if (this.promises[id]) {
-        if (isJsonRpcResponseError(payload)) {
-          this.promises[id].reject(payload.error)
-        } else if (isJsonRpcResponseSuccess(payload)) {
-          this.promises[id].resolve(payload.result)
-        }
-        delete this.promises[id]
-      }
-    } else if (isJsonRpcSubscription(payload)) {
-      if (payload.method && payload.method.indexOf('_subscription') > -1) {
-        // Emit subscription result
-        this.emit(payload.params.subscription, payload.params.result)
-        this.emit(payload.method, payload.params) // Latest EIP-1193
-        this.emit('data', payload) // Backwards Compatibility
-      }
-    }
+    this.setupChannelRpcStream()
   }
 
   enable() {
@@ -47,7 +25,9 @@ class TorusChannelProvider extends SafeEventEmitter {
         this._send('chan_config')
           .then(config => {
             if (Object.keys(config).length > 0) {
-              this.config = config
+              this._config = config
+              this._multisigAddress = config.multisigAddress
+              this._signerAddress = config.signerAddress
               this.emit('connect')
               resolve(config)
             } else {
@@ -63,10 +43,121 @@ class TorusChannelProvider extends SafeEventEmitter {
     })
   }
 
-  send() {
-    // Send can be clobbered, proxy sendPromise for backwards compatibility
-    return this._send(...arguments)
+  async send(method, params = {}) {
+    let result
+    switch (method) {
+      case 'chan_storeSet':
+        result = await this.set(params.pairs)
+        break
+      case 'chan_storeGet':
+        result = await this.get(params.path)
+        break
+      case 'chan_nodeAuth':
+        result = await this.signMessage(params.message)
+        break
+      case 'chan_config':
+        result = this.config
+        break
+      case 'chan_restoreState':
+        result = await this.restoreState(params.path)
+        break
+      default:
+        result = await this._send(method, params)
+        break
+    }
+    return result
   }
+
+  setupChannelRpcStream() {
+    const channelMux = setupMultiplex(this.connectionStream)
+    channelMux.setMaxListeners(20)
+
+    this.channelRpcStream = channelMux.getStream('channel_rpc')
+
+    this.channelRpcStream.on('data', payload => this.onPayload(payload))
+  }
+
+  async onPayload(payload) {
+    const { id } = payload
+    if (typeof id !== 'undefined') {
+      if (this.promises[id]) {
+        if (isJsonRpcResponseError(payload)) {
+          this.promises[id].reject(payload.error)
+        } else if (isJsonRpcResponseSuccess(payload)) {
+          this.promises[id].resolve(payload.result)
+        }
+        delete this.promises[id]
+      }
+    }
+  }
+
+  /// ///////////////
+  /// // GETTERS / SETTERS
+  get isSigner() {
+    return false
+  }
+
+  get config() {
+    return this._config
+  }
+
+  get multisigAddress() {
+    const multisigAddress = this._multisigAddress || (this._config ? this._config.multisigAddress : undefined)
+    return multisigAddress
+  }
+
+  set multisigAddress(multisigAddress) {
+    if (this._config) {
+      this._config.multisigAddress = multisigAddress
+    }
+    this._multisigAddress = multisigAddress
+  }
+
+  get signerAddress() {
+    return this._signerAddress
+  }
+
+  set signerAddress(signerAddress) {
+    this._signerAddress = signerAddress
+  }
+
+  /// ////////////////////////////////////////////
+  /// // LISTENER METHODS
+  on(event, listener) {
+    // dumb clients don't require listeners
+  }
+
+  once(event, listener) {
+    // dumb clients don't require listeners
+  }
+
+  /// ////////////////////////////////////////////
+  /// // SIGNING METHODS
+  async signMessage(message) {
+    return this._send('chan_nodeAuth', { message })
+  }
+
+  /// ////////////////////////////////////////////
+  /// // STORE METHODS
+  async get(path) {
+    return this._send('chan_storeGet', {
+      path
+    })
+  }
+
+  async set(pairs, allowDelete) {
+    return this._send('chan_storeSet', {
+      allowDelete,
+      pairs
+    })
+  }
+
+  async restoreState(path) {
+    return this._send('chan_restoreState', { path })
+  }
+
+  /// ////////////////////////////////////////////
+  /// // PRIVATE METHODS
 
   _send(method, params = {}) {
     if (!method || typeof method !== 'string') {
@@ -82,81 +173,23 @@ class TorusChannelProvider extends SafeEventEmitter {
     this.channelRpcStream.write(payload)
     return promise
   }
-
-  subscribe(type, method, params = []) {
-    return this._send(type, [method, ...params]).then(id => {
-      this.subscriptions.push(id)
-      return id
-    })
-  }
-
-  unsubscribe(type, id) {
-    return this._send(type, [id]).then(success => {
-      if (success) {
-        this.subscriptions = this.subscriptions.filter(_id => _id !== id) // Remove subscription
-        this.removeAllListeners(String(id)) // Remove listeners
-        return success
-      }
-    })
-  }
-
-  sendAsync(payload, cb) {
-    // Backwards Compatibility
-    if (!cb || typeof cb !== 'function') {
-      return cb(new Error('Invalid or undefined callback provided to sendAsync'))
-    }
-    if (!payload) {
-      return cb(new Error('Invalid Payload'))
-    }
-    // sendAsync can be called with an array for batch requests used by web3.js 0.x
-    // this is not part of EIP-1193's backwards compatibility but we still want to support it
-    if (payload instanceof Array) {
-      return this.sendAsyncBatch(payload, cb)
-    } else if (isJsonRpcRequest(payload)) {
-      return this._send(payload.method, payload.params)
-        .then(result => {
-          cb(null, { id: payload.id, jsonrpc: payload.jsonrpc, result })
-        })
-        .catch(err => {
-          cb(err)
-        })
-    }
-  }
-
-  sendAsyncBatch(requests, cb) {
-    return this._sendBatch(requests)
-      .then(results => {
-        const result = results.map((entry, index) => {
-          return {
-            id: requests[index].id,
-            jsonrpc: requests[index].jsonrpc,
-            result: entry
-          }
-        })
-        cb(null, result)
-      })
-      .catch(err => {
-        cb(err)
-      })
-  }
 }
 
 // util
 
-export function isJsonRpcSubscription(object) {
-  return typeof object.params === 'object'
-}
-
-export function isJsonRpcRequest(object) {
-  return typeof object.method !== 'undefined'
-}
-
-export function isJsonRpcResponseSuccess(object) {
+function isJsonRpcResponseSuccess(object) {
   return typeof object.result !== 'undefined'
 }
 
-export function isJsonRpcResponseError(object) {
+function isJsonRpcResponseError(object) {
   return typeof object.error !== 'undefined'
+}
+
+function payloadId() {
+  const datePart = new Date().getTime() * Math.pow(10, 3)
+  const extraPart = Math.floor(Math.random() * Math.pow(10, 3))
+  const id = datePart + extraPart
+  return id
 }
 
 export default TorusChannelProvider
