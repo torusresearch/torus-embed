f1c5f1342b5afde5087544085873bf8e88bb1001
chaitanyapotti <chaitanya.potti@gmail.com>
2020-04-13 12:12:11 +0800

    Use uppercase for packagename

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 const pkgName = 'torus'
+const libraryName = pkgName.charAt(0).toUpperCase() + pkgName.slice(1)
 
 const externals = ['@chaitanyapotti/random-id', '@toruslabs/fetch-node-details', '@toruslabs/torus.js', 'fast-deep-equal', 'loglevel']
 
@@ -11,7 +12,7 @@ const baseConfig = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     // filename: 'bundle.js',
-    library: pkgName,
+    library: libraryName,
     libraryExport: 'default',
     // libraryTarget: 'umd',
   },
