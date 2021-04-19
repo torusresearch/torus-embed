a4d882eac5d00351b594506730d37987f2cfae65
chaitanyapotti <chaitanya.potti@gmail.com>
2021-04-19 19:14:21 +0000

    Updating embed with new hash

--- src/embed.js
+++ src/embed.js
@@ -32,7 +32,7 @@ const defaultVerifiers = {
   [DISCORD]: true,
 }
 
-const iframeIntegrity = 'sha384-91KPWs7+Bx8YV6q08qMDrzVo5vs4B9ePtkdlO2vkHZpnihOo7e/3CXfa7XQE1E5c'
+const iframeIntegrity = 'sha384-H/cF/atWyk/VQOZ4ZbOod0faeuq6EQHOvOB1FI2ErcJCItcgKnojXKHdiz6Kwtgu'
 
 const expectedCacheControlHeader = 'max-age=3600'
 
