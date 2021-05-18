e14735ecf7c32f8540443ba0cac4f63d2b781212
chaitanyapotti <chaitanya.potti@gmail.com>
2021-05-18 17:22:48 +0800

    Lint and update deps

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
-git checkout binance && git pull
 git checkout master && git pull
 git checkout develop
-git merge binance
 git merge master --strategy-option theirs -m "Merged master"
 git push
